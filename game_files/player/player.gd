1dbd5135bbaf2f5961dccc03e25af8ade980b1a7
latterladdermatters <164977871+latterladdermatters@users.noreply.github.com>
2024-04-07 17:51:37 -0600

    Make lower_resultant_velocity() use a buffer for velocity values

--- game_files/player/player.gd
+++ game_files/player/player.gd
@@ -1,17 +1,17 @@
 extends CharacterBody3D
 
 #Variable for normal acceleration applied by the player
-@export var applied_normal_acceleration_scalar : Dictionary = {"x" = 20, "y" = 30, "z" = 20}
+@export var applied_normal_acceleration_scalar : Dictionary = {"x" = 20, "y" = 1000, "z" = 20}
 #Variable for added acceleration alongside normal acceleration (for larger jumps, quicker changes in direction, and so on)
 @export var applied_added_acceleration_scalar : Dictionary = {"x" = 20, "y" = 10, "z" = 20}
 
 # Variable for acceleration due to friction (calculated only when player is not "applying" any acceleration)
 @export var friction_acceleration_scalar : int = 25
 # Variable for acceleration due to gravity (on the y axis and calculated when player is not "applying" any acceleration along the y axis (as of writing this the player can only do this through jumping)
-@export var gravity_acceleration_scalar : int = 1
+@export var gravity_acceleration_scalar : int = 5
 
 # Variables for the maximum velocity for the player
-@export var max_velocity_scalar : Dictionary = {"x" = 8, "y" = 20, "z" = 8}
+@export var max_velocity_scalar : Dictionary = {"x" = 8, "y" = 100, "z" = 8}
 
 # Variable for the player's "net" acceleration
 var target_acceleration : Dictionary = {"x" = 0, "y" = 0, "z" = 0}
@@ -45,10 +45,8 @@ func calculate_ground_velocity(delta : float):
 		target_velocity["x"] = (target_acceleration["x"] * delta) + target_velocity["x"]
 		target_velocity["z"] = (target_acceleration["z"] * delta) + target_velocity["z"]
 		
-		print("resultant target velocity is " + str(pow(pow(target_velocity["x"], 2) + pow(target_velocity["z"], 2), (1/2.0))))
-		
 		if pow((pow(target_velocity["x"], 2) + pow(target_velocity["z"], 2)), 1/2.0) > max_velocity_scalar["x"]:
-			lower_resultant_velocity()
+			lower_resultant_velocity(target_velocity["x"], target_velocity["z"])
 
 func determine_ground_input(delta : float, plane : String, negative_input : String, positive_input : String, added_acceleration_negative : int, added_acceleration_positive : int):
 	var acceleration_to_determine : float
@@ -79,24 +77,36 @@ func calculate_friction(delta : float):
 		target_velocity["x"] = cos(atan2(target_velocity["z"], target_velocity["x"])) * (pow((pow(target_velocity["x"], 2) + pow(target_velocity["z"], 2)), 1/2.0) - (friction_acceleration_scalar * delta))
 		target_velocity["z"] = sin(atan2(target_velocity["z"], target_velocity["x"])) * (pow((pow(target_velocity["x"], 2) + pow(target_velocity["z"], 2)), 1/2.0) - (friction_acceleration_scalar * delta))
 
-func lower_resultant_velocity():
+func lower_resultant_velocity(current_x_velocity : float, current_z_velocity : float):
 	print("fixing diagonal velocity")
-	target_velocity["x"] = cos(atan2(target_velocity["z"], target_velocity["x"])) * max_velocity_scalar["x"]
-	target_velocity["z"] = sin(atan2(target_velocity["z"], target_velocity["x"])) * max_velocity_scalar["x"]
+	target_velocity["x"] = cos(atan2(current_z_velocity, current_x_velocity)) * max_velocity_scalar["x"]
+	target_velocity["z"] = sin(atan2(current_z_velocity, current_x_velocity)) * max_velocity_scalar["z"]
+	
+	print("direction is " + str(rad_to_deg(atan2(target_velocity["z"], target_velocity["x"]))))
+	
 
 func calculate_y_velocity(delta : float):
-	if (Input.is_action_pressed("jump")) and is_on_floor():
-		target_acceleration["y"] = applied_normal_acceleration_scalar["y"]
-	else:
+	if is_on_floor() == false:
 		target_acceleration["y"] = -(gravity_acceleration_scalar)
-		if is_on_floor():
-			# This represents the normal force
-			target_acceleration["y"] = 0
-			target_velocity["y"] = 0
-		elif abs(target_velocity["y"]) > max_velocity_scalar["y"]:
-			target_velocity["y"] = max_velocity_scalar["y"] * (abs(target_velocity["y"]) / target_velocity["y"])
 	
-	target_velocity["y"] += target_acceleration["y"]
+	if is_on_floor() == true:
+		target_acceleration["y"] = 0
+		target_velocity["y"] = 0
+	
+	#target_velocity["y"] = (target_acceleration["y"] * delta) + target_velocity["y"]
+	
+	if (Input.is_action_pressed("jump") == true and is_on_floor() == true):
+		target_acceleration["y"] = applied_normal_acceleration_scalar["y"]
+		#target_velocity["y"] += (applied_normal_acceleration_scalar["y"] * delta)
+	#elif (Input.is_action_pressed("jump") == true and target_velocity["y"] > 0 and target_velocity["y"] < 20):
+		#target_acceleration["y"] = applied_normal_acceleration_scalar["y"]
+	#elif (Input.is_action_pressed("jump") == true and target_velocity["y"] > 5):
+		#target_velocity["y"] = 0
+	
+	if abs((target_acceleration["y"] * delta) + target_velocity["y"]) > max_velocity_scalar["y"]:
+		target_velocity["y"] = (abs(target_velocity["y"])/target_velocity["y"]) * max_velocity_scalar["y"]
+	else:
+		target_velocity["y"] = (target_acceleration["y"] * delta) + target_velocity["y"]
 
 func _physics_process(delta):
 	print(str(is_on_floor()))
