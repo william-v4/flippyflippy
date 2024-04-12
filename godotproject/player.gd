95772d604d8fca7ecf24bc0e5c963e8acfc35d7c
William Zhang <77857686+william-v4@users.noreply.github.com>
2024-04-12 13:46:17 -0600

    robert's changes

--- godotproject/player.gd
+++ godotproject/player.gd
@@ -12,6 +12,8 @@ signal flip_platforms
 @export var friction_acceleration_scalar : int = 50
 # Variable for acceleration due to gravity (on the y axis and calculated when player is not "applying" any acceleration along the y axis (as of writing this the player can only do this through jumping)
 @export var gravity_acceleration_scalar : int = 5
+# Variable for acceleration added alongside the downwards acceleration from gravity (this is when the player holds the jump action key for longer, leaving the player up in the air for longer)
+@export var lower_gravity_scalar : int = 8
 
 # Variables for the maximum velocity for the player
 @export var max_velocity_scalar : Dictionary = {"x" = 8, "y" = 100, "z" = 8}
@@ -39,90 +41,93 @@ func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func calculate_ground_velocity(delta : float):
-	# For each plane, determine which keys are being pressed, determining the applied acceleration from the player.
+	# Determine the velocity of target_velocity on the x and z axis, respectively.
 	for plane in ["x", "z"]:
-		#if int(target_velocity[plane]) == 0 and int(target_acceleration[plane]) == 0:
-			#print("snapping " + str(plane) + " to 0")
-			#target_velocity[plane] == 0.0000000000000000000000000000000000000000000000000000000000000000000
-			#target_acceleration[plane] == 0.000000000000000000000000000000000000000000000
-		
-		if (target_velocity[plane] < 0.0) and (target_velocity[plane] >= -(max_velocity_scalar[plane])):
-			print(str(plane) + " velocity is less than 0 but more than or equal to negative_max")
-			determine_ground_input(delta, plane, input_negative_value[plane], input_positive_value[plane], 0, applied_added_acceleration_scalar[plane])
-		elif target_velocity[plane] == 0.0:
-			print(str(plane) + " velocity is 0")
-			determine_ground_input(delta, plane, input_negative_value[plane], input_positive_value[plane], 0, 0)
-		elif (target_velocity[plane] > 0.0) and (target_velocity[plane] <= max_velocity_scalar[plane]):
-			print(str(plane) + " velocity is more than 0 but less than or equal to positive_max")
-			determine_ground_input(delta, plane, input_negative_value[plane], input_positive_value[plane], -(applied_added_acceleration_scalar[plane]), 0)
+		if ((target_velocity[plane] < 0) and (target_velocity[plane] >= -(max_velocity_scalar[plane]))):
+			determine_ground_input(plane, 0, applied_added_acceleration_scalar[plane])
+		elif (target_velocity[plane] == 0):
+			determine_ground_input(plane, 0, 0)
+		elif ((target_velocity[plane] > 0) and (target_velocity[plane] <= max_velocity_scalar[plane])):
+			determine_ground_input(plane, -(applied_added_acceleration_scalar[plane]), 0)
 	
-	print(str(target_velocity))
-	print(str(target_acceleration))
+	# If the player is only applying acceleration on one plane, begin decelerating the other by (applied_normal_acceleration_scalar + applied_added_acceleration_scalar).
+	if (abs(target_acceleration["z"]) > 0) and (target_acceleration["x"] == 0):
+		decelerate_plane(delta, "x")
+	elif (abs(target_acceleration["x"]) > 0) and (target_acceleration["z"] == 0):
+		decelerate_plane(delta, "z")
+	# If the player is not applying acceleration on either plane, begin applying friction.
+	elif (pow((pow(target_acceleration["x"], 2) + pow(target_acceleration["z"], 2)), 1/2.0) == 0):
+		apply_friction(delta)
 	
-	# If the player is not applying acceleration, begin applying friction.
-	if pow((pow(target_acceleration["x"], 2) + pow(target_acceleration["z"], 2)), 1/2.0) == 0:
-		calculate_friction(delta)
-	else:
-		# Change the velocity by the target acceleration.
-		target_velocity["x"] = (target_acceleration["x"] * delta) + target_velocity["x"]
-		target_velocity["z"] = (target_acceleration["z"] * delta) + target_velocity["z"]
-		
-		# If the resultant velocity is above the maximum velocity scalar defined above, lower the resultant velocity while maintaining the same direction.
-		if pow((pow(target_velocity["x"], 2) + pow(target_velocity["z"], 2)), 1/2.0) > max_velocity_scalar["x"]:
-			lower_resultant_velocity(target_velocity["x"], target_velocity["z"])
-		
-		if pow((pow(target_acceleration["x"], 2) + pow(target_acceleration["z"], 2)), 1/2.0) > applied_normal_acceleration_scalar["x"]:
-			lower_resultant_acceleration(target_velocity["x"], target_velocity["z"])
+	# If the target_acceleration on both planes are accelerating at applied_normal_acceleration_scalar, then lower the resultant target_acceleration
+	#	to applied_normal_acceleration_scalar, while maintaining the same direction.
+	if pow((pow(target_acceleration["x"], 2) + pow(target_acceleration["z"], 2)), 1/2.0) == pow((pow(applied_normal_acceleration_scalar["x"], 2) + pow(applied_normal_acceleration_scalar["x"], 2)), 1/2.0):
+		lower_resultant_acceleration(target_acceleration["x"], target_acceleration["z"], applied_normal_acceleration_scalar["x"])
+	
+	# If the target_acceleration on one plane is applied_normal_acceleration_scalar, with the other one accelerating at the combined value of
+	#	applied_normal_acceleration_scalar and applied_added_acceleration_scalar, lower the resultant target_acceleration to the combined value
+	#	of applied_normal_acceleration_scalar and applied_added_acceleration_scalar, while maintaining the same direction.
+	if pow((pow(target_acceleration["x"], 2) + pow(target_acceleration["z"], 2)), 1/2.0) >= pow((pow(applied_normal_acceleration_scalar["x"], 2) + pow((applied_normal_acceleration_scalar["x"] + applied_added_acceleration_scalar["x"]), 2)), 1/2.0):
+		lower_resultant_acceleration(target_acceleration["x"], target_acceleration["z"], (applied_normal_acceleration_scalar["x"] + applied_added_acceleration_scalar["x"]))
+	
+	# Change the velocity by the target acceleration. Functions apply_friction() or decelerate_plane() have the ability to change target_velocity() of a given plane directly,
+	#	but in both cases, target_acceleration() for that plane is already set to 0, meaning there's no change after running these two commands.
+	target_velocity["x"] = (target_acceleration["x"] * delta) + target_velocity["x"]
+	target_velocity["z"] = (target_acceleration["z"] * delta) + target_velocity["z"]
+	
+	# If the resultant velocity is above the maximum velocity scalar defined above, lower the resultant velocity while maintaining the same direction.
+	if (pow((pow(target_velocity["x"], 2) + pow(target_velocity["z"], 2)), 1/2.0) > max_velocity_scalar["x"]):
+		lower_resultant_velocity(target_velocity["x"], target_velocity["z"])
 
-func determine_ground_input(delta : float, plane : String, negative_input : String, positive_input : String, added_acceleration_negative : int, added_acceleration_positive : int):
-	if Input.is_action_pressed(negative_input) == true:
+func determine_ground_input(plane : String, added_acceleration_negative : int, added_acceleration_positive : int):
+	if (Input.is_action_pressed(input_negative_value[plane])):
 		target_acceleration[plane] = (-(applied_normal_acceleration_scalar[plane]) + added_acceleration_negative)
-	elif Input.is_action_pressed(positive_input) == true:
+	elif (Input.is_action_pressed(input_positive_value[plane])):
 		target_acceleration[plane] = (applied_normal_acceleration_scalar[plane] + added_acceleration_positive)
-	#If neither key for the given plane is being pressed, then begin decelerating in that plane.
-	elif abs(target_velocity[plane]) > 0 and (abs(target_velocity[plane]) - ((applied_normal_acceleration_scalar[plane] + applied_added_acceleration_scalar[plane]) * delta)) < 0:
-		target_velocity[plane] = 0
-		target_acceleration[plane] = 0
-		print("snapped accel this, acceleration is now " + str(plane) + str(target_acceleration))
-	elif abs(target_velocity[plane]) > 0:
-		target_acceleration[plane] = (target_velocity[plane] / abs(target_velocity[plane]) * -1) * (applied_normal_acceleration_scalar[plane] + applied_added_acceleration_scalar[plane])
-		print("standard deccel this, acceleration is now " + str(plane) + str(target_acceleration))
+	# If neither key for the given plane is being pressed, set target_acceleration to 0 as an indicator to the remaining processes in the frame.
 	else:
 		target_acceleration[plane] = 0
-		print("else function, accel is " + str(target_acceleration))
 
-func calculate_friction(delta : float):
-	if (pow((pow(target_velocity["x"], 2) + pow(target_velocity["z"], 2)), 1/2.0) - (friction_acceleration_scalar * delta)) < 0:
+func lower_resultant_acceleration(current_x_acceleration : int, current_z_acceleration : int, maximum_acceleration : int):
+	target_acceleration["x"] = cos(atan2(current_z_acceleration, current_x_acceleration)) * maximum_acceleration
+	target_acceleration["z"] = sin(atan2(current_z_acceleration, current_x_acceleration)) * maximum_acceleration
+
+func apply_friction(delta : float):
+	# If changing target_velocity by the friction acceleration value will result in a non-zero value, set target_velocity and target_acceleration of that plane to 0.
+	if ((pow((pow(target_velocity["x"], 2) + pow(target_velocity["z"], 2)), 1/2.0) - (friction_acceleration_scalar * delta)) < 0):
 		target_velocity["x"] = 0
 		target_velocity["z"] = 0
 	else:
 		target_velocity["x"] = cos(atan2(target_velocity["z"], target_velocity["x"])) * (pow((pow(target_velocity["x"], 2) + pow(target_velocity["z"], 2)), 1/2.0) - (friction_acceleration_scalar * delta))
 		target_velocity["z"] = sin(atan2(target_velocity["z"], target_velocity["x"])) * (pow((pow(target_velocity["x"], 2) + pow(target_velocity["z"], 2)), 1/2.0) - (friction_acceleration_scalar * delta))
 
+func decelerate_plane(delta : float, plane_to_lower : String):
+	if ((abs(target_velocity[plane_to_lower]) - ((applied_normal_acceleration_scalar[plane_to_lower]) * delta)) < 0):
+		target_velocity[plane_to_lower] = 0
+	else:
+		target_acceleration[plane_to_lower] = (target_velocity[plane_to_lower] / abs(target_velocity[plane_to_lower]) * -1) * (applied_normal_acceleration_scalar[plane_to_lower])
+
 func lower_resultant_velocity(current_x_velocity : float, current_z_velocity : float):
 	target_velocity["x"] = cos(atan2(current_z_velocity, current_x_velocity)) * max_velocity_scalar["x"]
 	target_velocity["z"] = sin(atan2(current_z_velocity, current_x_velocity)) * max_velocity_scalar["z"]
 
-func lower_resultant_acceleration(current_x_acceleration : float, current_z_acceleration : float):
-	target_acceleration["x"] = cos(atan2(current_z_acceleration, current_x_acceleration)) * applied_normal_acceleration_scalar["x"]
-	target_acceleration["z"] = sin(atan2(current_z_acceleration, current_x_acceleration)) * applied_normal_acceleration_scalar["x"]
-
 func calculate_y_velocity(delta : float):
-	# If nothing else below this instruction changes the target acceleration along the y axis, the acceleration due to gravity will bring the player back to the ground.
+	# If nothing else below this instruction changes the target acceleration along the y axis, the acceleration due to gravity will be the only acceleration along the y axis, bringing the player back to the ground.
 	target_acceleration["y"] = -(gravity_acceleration_scalar)
-	
-	if is_on_floor() == true:
+
+	if (is_on_floor()):
 		target_acceleration["y"] = 0
 		target_velocity["y"] = 0
 	
+	# If the player is on the ground and presses the jump action key, begin a jump.
 	if (Input.is_action_pressed("jump") == true) and (is_on_floor() == true):
 		target_acceleration["y"] = applied_normal_acceleration_scalar["y"]
 	
 	# If the player continues to press the "jump" action after starting the jump from the floor, lower the (scalar) acceleration due to gravity to provide the player more time in the air.
 	if  (Input.is_action_pressed("jump") == true) and (is_on_floor() == false) and (target_velocity["y"] > 0):
-		target_acceleration["y"] = (-(gravity_acceleration_scalar)) + 15
+		target_acceleration["y"] = (-(gravity_acceleration_scalar)) + lower_gravity_scalar
 	
-	if abs((target_acceleration["y"] * delta) + target_velocity["y"]) > max_velocity_scalar["y"]:
+	if (abs((target_acceleration["y"] * delta) + target_velocity["y"]) > max_velocity_scalar["y"]):
 		target_velocity["y"] = (abs(target_velocity["y"])/target_velocity["y"]) * max_velocity_scalar["y"]
 	else:
 		target_velocity["y"] = (target_acceleration["y"] * delta) + target_velocity["y"]
@@ -141,7 +146,7 @@ func pauser():
 		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 # runs whenever input is received
-func _input(event):
+func _unhandled_input(event):
 	# camera movement with mouse
 	if event is InputEventMouseMotion and !paused:
 		# rotate the player left and right
@@ -150,29 +155,25 @@ func _input(event):
 		$Pivot/Marker3D.rotate_x(-deg_to_rad(event.relative.y) * mousesensy)
 		# make sure player doesn't break their neck (rotating over 90 degrees)
 		$Pivot/Marker3D.rotation_degrees.x = clamp($Pivot/Marker3D.rotation_degrees.x, -90, 90)
-	pauser()
 
 # runs continuously
 func _physics_process(delta):
-	print("")
-	print("")
-	print("start of new frame")
-	print("target accel at start of frame is " + str(target_acceleration))
+	pauser()
 	
-	calculate_ground_velocity(delta)
-	calculate_y_velocity(delta)
-	
-	# Apply the target velocity to the velocity variable to be used by move_and_slide().
-	velocity.x = target_velocity["x"]
-	velocity.y = target_velocity["y"]
-	velocity.z = target_velocity["z"]
-	
-	if Input.is_action_just_pressed("action_key_a"):
-		flip_platforms.emit()
-	
-	#print(str((-(target_velocity["x"]) / delta) * delta))
-	#print(str(target_velocity))
-	# stop movement when paused
 	if !paused:
+		calculate_ground_velocity(delta)
+		calculate_y_velocity(delta)
+		
+		# Apply the target velocity to the velocity variable to be used by move_and_slide().
+		velocity.x = target_velocity["x"]
+		velocity.y = target_velocity["y"]
+		velocity.z = target_velocity["z"]
+		
+		if Input.is_action_just_pressed("action_key_a"):
+			flip_platforms.emit()
+		
+		#print(str((-(target_velocity["x"]) / delta) * delta))
+		#print(str(target_velocity))
+		# stop movement when paused
 		move_and_slide()
 		checklookingup()
