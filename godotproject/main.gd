fa11b71692c520bc70743fa15c054efd10b7fe4f
William Zhang <77857686+william-v4@users.noreply.github.com>
2024-04-11 23:20:54 -0600

    finally done merging everything

---
+++ godotproject/main.gd
@@ -0,0 +1,20 @@
+extends Node
+# true if the level is rotated
+var rotated : bool = false
+# stops further rotation while the level is rotating
+var rotating : bool = false
+# runs continuously
+func _process(delta):
+	# tell the level it is moving
+	if $platforms/tutorialLVL/AnimationPlayer.is_playing():
+		rotating = true
+	else: 
+		rotating = false
+
+func _input(event):
+	if Input.is_action_just_pressed("action_key_a") and !rotated and !rotating:
+		$platforms/tutorialLVL/AnimationPlayer.play("rotatetoparallel")
+		rotated = true
+	if Input.is_action_just_pressed("action_key_a") and rotated and !rotating: 
+		$platforms/tutorialLVL/AnimationPlayer.play_backwards("rotatetoparallel")
+		rotated = false
