d344b20158e2f85d74f59f555bc142362120d457
latterladdermatters <164977871+latterladdermatters@users.noreply.github.com>
2024-04-12 21:39:31 -0600

    Rename platforms of Main scene to Platforms

--- godotproject/main/main.gd
+++ godotproject/main/main.gd
@@ -6,15 +6,15 @@ var rotating : bool = false
 # runs continuously
 func _process(delta):
 	# tell the level it is moving
-	if get_node("platforms/tutorialLVL/AnimationPlayer").is_playing():
+	if get_node("Platforms/tutorialLVL/AnimationPlayer").is_playing():
 		rotating = true
 	else: 
 		rotating = false
 
 func _input(event):
 	if Input.is_action_just_pressed("action_key_a") and !rotated and !rotating:
-		get_node("platforms/tutorialLVL/AnimationPlayer").play("rotatetoparallel")
+		get_node("Platforms/tutorialLVL/AnimationPlayer").play("rotatetoparallel")
 		rotated = true
 	if Input.is_action_just_pressed("action_key_a") and rotated and !rotating: 
-		get_node("platforms/tutorialLVL/AnimationPlayer").play_backwards("rotatetoparallel")
+		get_node("Platforms/tutorialLVL/AnimationPlayer").play_backwards("rotatetoparallel")
 		rotated = false
